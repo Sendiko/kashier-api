296ec82c50c0c0596439c7a24ac0e669e0fb9584
Sendiko <sarangtawon897@gmail.com>
2025-10-11 12:06:18 +0700

    docs: add api documentation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import express, { Application, Request, Response } from "express";
 import config from './config/config.js';
 import syncModels from "models/sync.js";
 import router from "route/route.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 await syncModels();
 
 const PORT: number = parseInt(config.PORT);
@@ -10,13 +16,13 @@ const IP: string = "localhost";
 const app: Application = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, Kashier Client!");
-});
+  res.sendFile(path.join(__dirname, "../views/index.html"));
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(PORT, IP, () => {
-  console.log(`Hello World ${IP}:${PORT}`);
+  console.log(`Hello World ${IP}:${PORT}`);
 });
