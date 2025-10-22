5358c42ac5fad67585065c6d27fc65ec2b1c2b12
Sendiko <rizkysendiko7@gmail.com>
2025-10-22 17:35:49 +0700

    feat: create admin controller for login

---
+++ src/controller/admin_controller.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import Admin from "models/admin.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import config from "config/config.js";
+
+const AdminController = {
+  login: async (req: Request, res: Response) => {
+    try {
+      const admin = await Admin.findOne({
+        where: {
+          name: req.body.name,
+        },
+      });
+
+      if (!admin) {
+        return res.status(404).json({
+          status: 404,
+          message: "Admin not found.",
+        });
+      }
+
+      const match = await bcrypt.compare(req.body.password, admin.password);
+      if (!match) {
+        return res.status(401).json({
+          status: 401,
+          message: "Password not matched.",
+        });
+      }
+
+      const token = jwt.sign({ name: admin.name }, `${config.JWTAUTHKEY}`);
+
+      return res.status(200).json({
+        status: 200,
+        message: "Admin successfully logged in.",
+      });
+    } catch (error: any) {
+      return res.status(500).json({
+        status: 500,
+        message: error.message,
+      });
+    }
+  },
+};
