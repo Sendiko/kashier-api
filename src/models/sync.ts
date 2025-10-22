e44e259f68e136c485d7bb8e31c58994f5fdeee7
Sendiko <rizkysendiko7@gmail.com>
2025-10-22 17:36:04 +0700

    feat: fix password creation with bcrypt

--- src/models/sync.ts
+++ src/models/sync.ts
@@ -2,13 +2,15 @@ import Admin from "./admin.js";
 import History from "./history.js";
 import Item from "./item.js";
 import User from "./user.js";
+import bcrypt from "bcrypt";
 
 const syncModels = async () => {
   try {
     await Admin.sync();
     const admins = await Admin.findAll();
     if (admins.length === 0) {
-      await Admin.create({ name: "admin", password: "admin123" });
+      const password = bcrypt.hash("admin123", 10);
+      await Admin.create({ name: "admin", password: password });
     }
     User.hasMany(Item, { foreignKey: "userId"})
     User.hasMany(History, { foreignKey: "userId"})
