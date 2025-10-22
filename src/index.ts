5852df711c4e5bf05fbd71f7db4cc71b405210a7
Sendiko <sarangtawon897@gmail.com>
2025-10-22 18:55:53 +0700

    fix: cors allow credentials

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,9 @@ app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "../views/index.html"));
 })
 
-app.use(cors);
+app.use(cors({
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routerv1);
