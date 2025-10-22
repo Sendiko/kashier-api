d24ea235ef4d96c00c427d06afe475bf98368e2b
Sendiko <sarangtawon897@gmail.com>
2025-10-22 17:17:08 +0700

    feat: seed admin

--- src/models/sync.ts
+++ src/models/sync.ts
@@ -1,9 +1,15 @@
+import Admin from "./admin.js";
 import History from "./history.js";
 import Item from "./item.js";
 import User from "./user.js";
 
 const syncModels = async () => {
   try {
+    await Admin.sync();
+    const admins = await Admin.findAll();
+    if (admins.length === 0) {
+      await Admin.create({ name: "admin", password: "admin123" });
+    }
     User.hasMany(Item, { foreignKey: "userId"})
     User.hasMany(History, { foreignKey: "userId"})
     await User.sync();
