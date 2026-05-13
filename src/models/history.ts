c5099a0b54fb62565f8573d42d2e8b9726e14a6a
Sendiko <rizkysendiko7@gmail.com>
2026-05-13 19:04:30 +0700

    fix: history's userId saved as null

--- src/models/history.ts
+++ src/models/history.ts
@@ -15,6 +15,14 @@ History.init(
       primaryKey: true,
       autoIncrement: true
     },
+    userId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id'
+      }
+    },
     total: {
       type: DataTypes.FLOAT,
       allowNull: false
